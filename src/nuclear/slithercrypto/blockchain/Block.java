e8ece4a6dfa41425290db0ac0c89fa0725514e1b
NuclearManD <dylanbrophy@gmail.com>
2017-12-19 23:02:52 -0800

    yep!

--- src/nuclear/slithercrypto/blockchain/Block.java
+++ src/nuclear/slithercrypto/blockchain/Block.java
@@ -65,7 +65,7 @@ public class Block {
 	public boolean verify() {
 		byte[] packed=packNoHash();
 		valid=Arrays.equals(Crypt.SHA256(packed),hash);
-		valid=valid&&(difficulty.compareTo(new uint256_t(hash))>0);
+		valid=valid&&(difficulty.compareTo(new uint256_t(hash))>0);// yep
 		return valid;
 	}
 	public boolean mineOnce(byte[] publicKey) {
