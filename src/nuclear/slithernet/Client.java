c83d4c41cc3aa07f83f5961938d264947043ee52
NuclearManD <dylanbrophy@gmail.com>
2017-12-23 18:47:43 -0800

    fixed single-use Client glitch

--- src/nuclear/slithernet/Client.java
+++ src/nuclear/slithernet/Client.java
@@ -10,15 +10,19 @@ public class Client {
 	protected DataInputStream inStream;
 	protected DataOutputStream outStream;
 	protected Socket socket;
+	private String host;
+	private int port;
 	public Client(int port, String host) throws IOException{
-		socket=new Socket(host, port);
-		inStream=new DataInputStream(socket.getInputStream());
-		outStream=new DataOutputStream(socket.getOutputStream());
+		this.host=host;
+		this.port=port;
 	}
 	public String ezPoll(String in) throws IOException {
 		return new String(poll(in.getBytes(StandardCharsets.UTF_8)),StandardCharsets.UTF_8);
 	}
 	public byte[] poll(byte[] in) throws IOException {
+		socket=new Socket(host, port);
+		inStream=new DataInputStream(socket.getInputStream());
+		outStream=new DataOutputStream(socket.getOutputStream());
 		outStream.writeLong(in.length);
 		outStream.write(in);
 		outStream.flush();
@@ -27,6 +31,7 @@ public class Client {
 		if(len>0) {
 			inStream.readFully(out, 0, (int)len);
 		}
+		socket.close();
 		return out;
 	}
 }
