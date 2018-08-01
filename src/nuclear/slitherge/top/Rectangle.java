c385f0c2b9a73267e6cee1bc4b2981aa24e5ad1d
NuclearManD <dylanbrophy@gmail.com>
2018-08-01 12:26:25 -0400

    rectangle:use doubles!

--- src/nuclear/slitherge/top/Rectangle.java
+++ src/nuclear/slitherge/top/Rectangle.java
@@ -1,31 +1,23 @@
 package nuclear.slitherge.top;
 
-import java.awt.Dimension;
-import java.awt.Point;
-
 public class Rectangle extends java.awt.Rectangle {
+	public double width, height, x,y;
 	public Rectangle(java.awt.Rectangle r) {
-		super(r);
+		width=r.getWidth();
+		height=r.getHeight();
+		x=r.getX();
+		y=r.getY();
+	}
+	public Rectangle(double width, double height) {
+		this.width=width;
+		this.height=height;
 	}
 
-	public Rectangle(Point p) {
-		super(p);
-	}
-
-	public Rectangle(Dimension d) {
-		super(d);
-	}
-
-	public Rectangle(int width, int height) {
-		super(width, height);
-	}
-
-	public Rectangle(Point p, Dimension d) {
-		super(p, d);
-	}
-
-	public Rectangle(int x, int y, int width, int height) {
-		super(x, y, width, height);
+	public Rectangle(double x, double y, double width, double height) {
+		this.width=width;
+		this.height=height;
+		this.x=x;
+		this.y=y;
 	}
 	public boolean inRect(double degrees, Position p){
 		p=p.sub(new Position(p.dim,x,y)).rotate(-degrees).add(new Position(p.dim,x,y));
