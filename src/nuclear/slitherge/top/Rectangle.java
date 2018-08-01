a71086d187e21fbdc8d0590fb17a2ff43767b83a
NuclearManD <dylanbrophy@gmail.com>
2018-08-01 12:42:17 -0400

    oopsfix

--- src/nuclear/slitherge/top/Rectangle.java
+++ src/nuclear/slitherge/top/Rectangle.java
@@ -1,6 +1,6 @@
 package nuclear.slitherge.top;
 
-public class Rectangle extends java.awt.Rectangle {
+public class Rectangle {
 	public double width, height, x,y;
 	public Rectangle(java.awt.Rectangle r) {
 		width=r.getWidth();
@@ -26,5 +26,24 @@ public class Rectangle extends java.awt.Rectangle {
 	public boolean inRect(Position p){
 		p=p.sub(new Position(p.dim,x,y));
 		return p.x<=width&&p.x>=0&&p.y<=height&&p.y>=0;
+	}
+	public double getWidth() {
+		return width;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public double getX() {
+		return x;
+	}
+	public double getY() {
+		return y;
+	}
+	public boolean intersects(Rectangle hitbox) {
+		if (hitbox.x > (x+width) || x > (hitbox.x+hitbox.width))
+	        return false;
+		if (hitbox.y > (y+height) || y > (hitbox.y+hitbox.height))
+	        return false;
+		return true;
 	}
 }
